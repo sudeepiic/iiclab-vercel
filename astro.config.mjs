a980eaae1aaf8eb4ac2911d6b305173e083dda43
Ayush@270901 <ayush.shrivastav.dev.iic@gmail.com>
2023-12-03 20:32:32 +0530

    add: solution detail page fix: ui fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import {
     defineConfig
 } from 'astro/config';
-import react from '@astrojs/react'
 
 
 
@@ -9,7 +8,7 @@ import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react()],
+    integrations: [],
     // image: {
     //     domains: ["images.ctfassets.net"],
     // }
